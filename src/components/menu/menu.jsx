e7af6f9c546501c6e4b522785709b7947b6acebc
armaan4658 <armaan4658@gmail.com>
2021-09-27 21:33:53 +0530

    added resume

--- src/components/menu/menu.jsx
+++ src/components/menu/menu.jsx
@@ -1,8 +1,10 @@
 import './menu.scss';
+import Link from '@mui/material/Link';
 export const Menu = ({menuOpen,setMenuOpen}) => {
     const closeMenu = () => {
         setMenuOpen(false);
     }
+    const resume = `https://drive.google.com/file/d/1ZwrDZHeE9YAYxEOsnumJgD83Pvx6H9zn/view?usp=sharing`;
     return(
         <div className={menuOpen? "menu active" : "menu"}>
             <ul>
@@ -17,6 +19,9 @@ export const Menu = ({menuOpen,setMenuOpen}) => {
                 </li>
                 <li onClick={closeMenu}>
                     <a href="#contact">Contact</a>
+                </li>
+                <li onClick={closeMenu}>
+                    <Link onClick={()=>window.open(resume,"_blank")}>Resume</Link>
                 </li>
             </ul>
         </div>
