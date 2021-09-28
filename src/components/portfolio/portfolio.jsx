e37733f031291791d4086154603adc376e55c91b
armaan4658 <armaan4658@gmail.com>
2021-09-28 09:24:51 +0530

    added cards layout

--- src/components/portfolio/portfolio.jsx
+++ src/components/portfolio/portfolio.jsx
@@ -1,11 +1,44 @@
 import './portfolio.scss'
-import {Slider} from '../slider/Slider.js';
+import { IconButton } from '@mui/material';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import OpenInNewIcon from '@mui/icons-material/OpenInNew';
+// import {Slider} from '../slider/Slider.js';
+import {data} from '../../data';
 export const Portfolio = () => {
+    const openGithub = (d) => {
+        window.open(d,"_blank");
+    }
+    const openWebSite = (d) => {
+        window.open(d,"_blank");
+    }
     return(
         <div className="portfolio" id="portfolio">
             <h1>Portfolio</h1>
-            <div>
+            {/* <div>
                 <Slider/>
+            </div> */}
+            <div>
+                {data.map(d=>(
+                    <div style={{
+                        backgroundImage:`url(${d.src})`
+                    }}>
+                        <h2> {d.name} </h2>
+                        <p> {d.description} </p>
+                        <ul>
+                            {d.tech.map(t=>(
+                                <li> {t} </li>
+                            ))}
+                        </ul>
+                        <span>
+                                <IconButton onClick={()=>openGithub(d.github)}>
+                                    <GitHubIcon/>
+                                </IconButton>
+                                <IconButton onClick={()=>openWebSite(d.url)}>
+                                    <OpenInNewIcon/>
+                                </IconButton>
+                        </span>
+                    </div>
+                ))}
             </div>
         </div>
     )
